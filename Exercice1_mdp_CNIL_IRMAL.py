ff6528ba7d04ea825502be44fe81f82b9cb92241
Fares1611 <faresi1611@gmail.com>
2025-06-03 09:30:39 +0200

    Update Exercice1_mdp_CNIL_IRMAL.py

--- Exercice1_mdp_CNIL_IRMAL.py
+++ Exercice1_mdp_CNIL_IRMAL.py
@@ -2,34 +2,27 @@
 import string 
 
 def verifier_mot_de_passe_cnil():
-    # Demande à l'utilisateur de saisir un mot de passe.
     while True:
         mot_de_passe = input("Veuillez saisir un mot de passe : ")
         erreurs = []
-
-        # Longueur (12 à 14 caractères)
+        
         if not 12 <= len(mot_de_passe) <= 14:
             erreurs.append("Le mot de passe doit contenir entre 12 et 14 caractères.")
 
-        # Au moins une lettre majuscule
         if not any(char in string.ascii_uppercase for char in mot_de_passe):
             erreurs.append("Le mot de passe doit contenir au moins une lettre majuscule.")
 
-        # Au moins une lettre minuscule
         if not any(char in string.ascii_lowercase for char in mot_de_passe):
             erreurs.append("Le mot de passe doit contenir au moins une lettre minuscule.")
 
-        # Au moins un chiffre
         if not any(char in string.digits for char in mot_de_passe):
             erreurs.append("Le mot de passe doit contenir au moins un chiffre.")
 
-        # Au moins un caractère spécial
         caracteres_speciaux_a_verifier = "!@#$%^&*(),.?\":{}|<>" # Liste exemple de la CNIL
 
         if not any(char in caracteres_speciaux_a_verifier for char in mot_de_passe):
             erreurs.append(f"Le mot de passe doit contenir au moins un caractère spécial (ex: {caracteres_speciaux_a_verifier}).")
 
-        # Éviter les caractères ambigus
         caracteres_ambigus = ['l', '1', 'i', 'I', '0', 'O']
         for char_ambigu in caracteres_ambigus:
             if char_ambigu in mot_de_passe:
