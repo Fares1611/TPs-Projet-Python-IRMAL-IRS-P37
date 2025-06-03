998dcb1258c61f68040819983b631d0fed9659b6
Fares1611 <faresi1611@gmail.com>
2025-06-03 09:33:37 +0200

    Update Exercice4_Regex_IPV4_lecture_fichier_IRMAL.py

--- Exercice4_Regex_IPV4_lecture_fichier_IRMAL.py
+++ Exercice4_Regex_IPV4_lecture_fichier_IRMAL.py
@@ -1,3 +1,4 @@
+#Dans ce code le script lis un fichier texte contenant des adresses et le script vérifie s'il respecte le format IPv4
 import re
 
 ipv4_patron = r"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
