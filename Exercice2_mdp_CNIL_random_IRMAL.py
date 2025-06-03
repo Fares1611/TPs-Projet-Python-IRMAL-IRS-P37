6bbf054e4f31659888006d30e7d7c9aba23c1bdc
Fares1611 <faresi1611@gmail.com>
2025-06-03 09:31:22 +0200

    Update Exercice2_mdp_CNIL_random_IRMAL.py

--- Exercice2_mdp_CNIL_random_IRMAL.py
+++ Exercice2_mdp_CNIL_random_IRMAL.py
@@ -17,29 +17,24 @@ pool_total_secure = liste_maj_secure + liste_min_secure + liste_num_secure + lis
 
 def generer_mot_de_passe_cnil():
 
-    # Choisir une longueur aléatoire entre 12 et 14
     longueur_mdp = random.randint(12, 14)
     
     mot_de_passe_en_creation = []
 
-    # S'assurer d'avoir au moins un caractère de chaque type sécurisé
     mot_de_passe_en_creation.append(random.choice(liste_maj_secure))
     mot_de_passe_en_creation.append(random.choice(liste_min_secure))
     mot_de_passe_en_creation.append(random.choice(liste_num_secure))
     mot_de_passe_en_creation.append(random.choice(liste_speciaux_secure))
 
-    # Compléter le reste du mot de passe avec des caractères du pool sécurisé
     nombre_caracteres_restants = longueur_mdp - len(mot_de_passe_en_creation)
     for _ in range(nombre_caracteres_restants):
         mot_de_passe_en_creation.append(random.choice(pool_total_secure))
 
-    # Mélanger les caractères
     random.shuffle(mot_de_passe_en_creation)
     
     mdp_genere = "".join(mot_de_passe_en_creation)
     return mdp_genere
 
-# Programme principal
 if __name__ == "__main__":
     mot_de_passe_final = generer_mot_de_passe_cnil()
     print(f"\nMot de passe généré : {mot_de_passe_final}")
